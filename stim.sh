923fc1097de6c6d10a887ff502f5350e250d7157
Nate Marsella <nmarsell@redhat.com>
2019-04-17 08:12:54 -0400

    Update test file

---
+++ stim.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# Stimulate repo
+
+BRANCH=cvpco-$(uuidgen -r | cut -d '-' -f 1)
+echo $BRANCH
+
+REPO=/home/nmarsell/workspace/cvp-community-operators
+cd $REPO
+
+operators=(`ls | xargs`)
+
+OPERATOR=${operators[$RANDOM % ${#operators[@]} ]}
+
+git checkout master
+git pull origin master
+git checkout -b $BRANCH
+
+echo $(date -uIs) > ${OPERATOR}/TEST.txt
+
+git add .
+git commit -m "Update test file"
+git push origin $BRANCH
